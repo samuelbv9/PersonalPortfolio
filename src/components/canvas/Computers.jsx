5134317865fff54dbb7837b893d3273a5249450c
sbechar <sbechar@umich.edu>
2023-06-24 11:54:49 -0400

    completed 3d avatar of myself

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { Suspense, useEffect, useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { meshBounds, OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_mac/avatar.gltf')
+
   return (
-    <div>Computers</div>
+    <mesh> 
+      <hemisphereLight intensity={0.15}
+      groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight 
+        position={[10,20,-10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale = {6}
+        position = {[-2, -10, 0]}
+        rotation = {[0.01,0.01,0.01]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop = "demand"
+      shadows
+      camera={{position: [20,3,-5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+        enableZoom={false}
+        maxPolarAngle = {Math.PI / 2}
+        minPolarAngle = {Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas;
