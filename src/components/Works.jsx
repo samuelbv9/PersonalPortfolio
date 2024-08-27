185153a7313deec5ff1782664d82c144e9b93adc
sbechar <sbechar@umich.edu>
2024-08-27 18:21:47 -0400

    test5

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,14 +10,14 @@ import { fadeIn, textVariant } from "../utils/motion"
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return (
     <motion.div variants={fadeIn("up", "spring",index*0.5,0.75)}>
-      <Tilt
-        options={{
-          max:45,
-          scale: 1,
-          speed: 450
-        }}
-        className = "bg-tertiary p-5 rounded-2xl sm:w-[360px]"
-      >
+      // <Tilt
+      //   options={{
+      //     max:45,
+      //     scale: 1,
+      //     speed: 450
+      //   }}
+      //   className = "bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      // >
         <div className="relative w-full h-[230px]">
           <img
             src={image}
@@ -51,7 +51,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
             </p>
           ))}
         </div>
-      </Tilt>
+      // </Tilt>
     </motion.div>
   );
 };
